90634793992ddfb3999eae83cb7a13c7e031eaed
Rahul N <rahul.nagarajmv@gmail.com>
2020-04-04 16:19:57 +0530

    setup server and other dependencies

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+const typeDefs = `
+	type Query {
+		info: String!
+	}
+`
+
+const resolvers = {
+	Query: {
+		info: () => `This is info query`,
+	},
+}
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers,
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
