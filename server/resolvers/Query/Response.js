8fdbe7962b01b6edbc2702d0c4b68756ff4a0ad1
Rahul N <rahul.nagarajmv@gmail.com>
2020-04-10 02:44:17 +0530

    calculate votes by question

--- server/resolvers/Query/Response.js
+++ server/resolvers/Query/Response.js
@@ -1,18 +1,53 @@
+import { groupBy, maxBy } from 'lodash'
+
 import { Response } from '../../models/Response'
 
-const getResponseBySurveyId = async (_, { id }) => {
-	const response = await Response.find({ survey_id: id })
+const getResponseBySurveyId = async (_, { survey_id }) => {
+	const response = await Response.find({ survey_id })
 	return response
 }
 
-const getResponseByQuestionId = async (_, { id }) => {
-	const response = await Response.find({ question_id: id })
+const getResponseByQuestionId = async (_, { question_id }) => {
+	const response = await Response.find({ question_id })
 	return response
 }
 
-const calculateVotes = async () => {}
+const calculateVotesByQuestion = async (_, { question_id }) => {
+	const response = await getResponseByQuestionId(_, { question_id })
+	const userCount = await Response.aggregate([
+		{ $group: { _id: '$user_id' } },
+		{ $count: 'user_id' },
+	])
+
+	// Group by response
+	const res = groupBy(response, 'response')
+
+	// Convert into frequency table
+	const frequencyTable = Object.keys(res).map((freq) => ({
+		option: freq,
+		count: res[freq].length,
+	}))
+
+	// Calculate the max vote
+	const winner = maxBy(frequencyTable, (freq) => freq.count)
+
+	// TODO: Handle equal votes. Default -> alphabetical
+
+	return [
+		{
+			question_id,
+			users_count: userCount[0].user_id,
+			votes: obj,
+			winner: winner.option,
+		},
+	]
+}
+
+const calculateVotesBySurvey = async (_, { survey_id }) => {}
 
 export default {
 	getResponseByQuestionId,
 	getResponseBySurveyId,
+	calculateVotesByQuestion,
+	calculateVotesBySurvey,
 }
